42d66d1f763277310d0f20de32fceddaf02683cf
jpjananraja <jpjananraja@gmail.com>
2015-01-21 19:54:22 +0530

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Janan Rajaratnam on 1/21/15.
+//  Copyright (c) 2015 Janan Rajaratnam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Janan Rajaratnam on 1/21/15.
+//  Copyright (c) 2015 Janan Rajaratnam. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
